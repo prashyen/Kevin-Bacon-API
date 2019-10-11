c16ed0f37e7f7ae416e1a8ed3ebd831abe944888
prashyen <37853402+prashyen@users.noreply.github.com>
2019-10-10 23:49:06 -0400

    Added 400 error to addrelationship

--- starter_code/src/main/java/ca/utoronto/utm/mcs/AddRelationship.java
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/AddRelationship.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class AddRelationship implements HttpHandler {
+public class AddRelationship implements HttpHandler, AutoCloseable {
 
 
     private static Driver driver;
@@ -39,16 +39,37 @@ public class AddRelationship implements HttpHandler {
 
     public void handlePost(HttpExchange r) throws IOException, JSONException {
         try {
+            Record result;
+            String aName = null, mName = null;
             String body = Utils.convert(r.getRequestBody());
             JSONObject deserialized = new JSONObject(body);
             String mID = deserialized.getString("movieId");
             String aID = deserialized.getString("actorId");
             System.out.println(mID+"  "+aID);
-            addRelationship(mID, aID, driver);
-            r.sendResponseHeaders(200, -1);
-            OutputStream os = r.getResponseBody();
-            os.write(-1);
-            os.close();
+            Map<String,Object> params = new HashMap<String,Object>();
+            params.put( "aID", aID );
+            params.put( "mID", mID );
+            try(Session CREATEsession = driver.session()) {
+                String query = "MATCH (a:Actor{actorId:{aID}}),(m:Movie{movieId:{mID}}) MERGE (a)-[:ACTED_IN]-(m) RETURN a.name, m.name";
+                StatementResult statementResult = CREATEsession.run(query, params);
+                while (statementResult.hasNext()) {
+                    result = statementResult.next();
+                    Map<String, Object> data = result.asMap();
+                    aName = (String) data.get("a.name");
+                    mName = (String) data.get("m.name");
+                }
+                if(aName != null && mName != null){
+                    r.sendResponseHeaders(200, -1);
+                    OutputStream os = r.getResponseBody();
+                    os.write(-1);
+                    os.close();
+                }else{
+                    r.sendResponseHeaders(400, -1);
+                    OutputStream os = r.getResponseBody();
+                    os.write(-1);
+                    os.close();
+                }
+            }
         }catch (JSONException e){
             r.sendResponseHeaders(400, -1);
             OutputStream os = r.getResponseBody();
@@ -63,17 +84,9 @@ public class AddRelationship implements HttpHandler {
         }
     }
 
-    private static void addRelationship(String mID, String aID, Driver driver) throws Exception {
-        try{
-            Map<String,Object> params = new HashMap<String,Object>();
-            params.put( "aID", aID );
-            params.put( "mID", mID );
-            Session CREATEsession = driver.session();
-            String query ="MATCH (a:Actor{actorId:{aID}}),(m:Movie{movieId:{mID}}) MERGE (a)-[:ACTED_IN]-(m) RETURN a.name";
-            StatementResult sr = CREATEsession.run( query, params);
-            System.out.print(sr.summary());
-        }catch(Exception e){
-            throw new Exception();
-        }
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
     }
 }
